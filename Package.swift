230b00bca768e0c19d93ff3455013f8c93e390b4
Jed Gyeong <jed@sendbird.com>
2024-12-02 15:36:33 +0900

    1.0.2

--- Package.swift
+++ Package.swift
@@ -10,9 +10,9 @@ let package = Package(
         .library(
             name: "SPMUIKit",
             targets: ["SPMUIKitTarget"]),
-//        .library(
-//            name: "SPMTemplate",
-//            targets: ["SPMTemplate"]),
+        .library(
+            name: "SPMTemplate",
+            targets: ["SPMTemplateTarget"]),
     ],
     dependencies: [
         .package(
@@ -21,20 +21,30 @@ let package = Package(
         ),
     ],
     targets: [
-//        .binaryTarget(
-//            name: "SPMTemplate",
-//            url: "https://github.com/sf-jed-kyung/spm-modules/releases/download/1.0.0/SPMTemplate.xcframework.zip",
-//            checksum: "2263767c49e2b51aa1368362269178fe8096f9152e7c00024ab8382960a2f4e2"
-//        ),
+        .binaryTarget(
+            name: "SPMTemplate",
+            url: "https://github.com/sf-jed-kyung/spm-modules/releases/download/1.0.2/SPMTemplate.xcframework.zip",
+            checksum: "577162bfe0a691828a8008ee3875e69b71924e5d2ba1ebe6456f768a24aa1842"
+        ),
         .binaryTarget(
             name: "SPMUIKit",
-            url: "https://github.com/sf-jed-kyung/spm-modules/releases/download/1.0.1/SPMUIKit.xcframework.zip",
+            url: "https://github.com/sf-jed-kyung/spm-modules/releases/download/1.0.2/SPMUIKit.xcframework.zip",
             checksum: "2333a1e69ba4d32db656bd13cac6fe49886f3741fae76437d8f9eeebce1c1704"
         ),
+        
         .target(
             name: "SPMUIKitTarget",
             dependencies: [
                 .target(name: "SPMUIKit"),
+                .target(name: "SPMTemplate"),
+                .product(name: "SPMChat", package: "spm-chat")
+            ],
+            path: "Framework/Dependency"
+        ),
+        
+        .target(
+            name: "SPMTemplateTarget",
+            dependencies: [
                 .product(name: "SPMChat", package: "spm-chat")
             ],
             path: "Framework/Dependency"
