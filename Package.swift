c9328b7c5f9b7844ba5733dfabcad39876ee1adf
Jed Gyeong <jed@sendbird.com>
2024-12-02 15:17:43 +0900

    initial commit.

---
+++ Package.swift
@@ -0,0 +1,47 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SPMModules",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SPMChat",
+            targets: ["SPMChatTarget"]),
+        .library(
+            name: "SPMUIKit",
+            targets: ["SPMUIKit"]),
+//        .library(
+//            name: "SPMTemplate",
+//            targets: ["SPMTemplate"]),
+    ],
+    dependencies: [
+        .package(
+            name: "SPMChat",
+            url: "https://github.com/sf-jed-kyung/spm-chat",
+            from: "1.0.1"
+        ),
+    ],
+    targets: [
+//        .binaryTarget(
+//            name: "SPMTemplate",
+//            url: "https://github.com/sf-jed-kyung/spm-modules/releases/download/1.0.0/SPMTemplate.xcframework.zip",
+//            checksum: "2263767c49e2b51aa1368362269178fe8096f9152e7c00024ab8382960a2f4e2"
+//        ),
+        .binaryTarget(
+            name: "SPMUIKit",
+            url: "https://github.com/sf-jed-kyung/spm-modules/releases/download/1.0.0/SPMUIKit.xcframework.zip",
+            checksum: "2263767c49e2b51aa1368362269178fe8096f9152e7c00024ab8382960a2f4e2"
+        ),
+        .target(
+            name: "SPMChatTarget",
+            dependencies: [
+                .target(name: "SPMChat"),
+                .product(name: "SPMChat", package: "SPMChat")
+            ],
+            path: "Framework/Dependency"
+        ),
+    ]
+)
