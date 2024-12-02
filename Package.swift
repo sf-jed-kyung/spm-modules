d72a021e8eac73698e0c15b73dc8bb8780fc46a5
Jed Gyeong <jed@sendbird.com>
2024-12-02 15:51:03 +0900

    Updated version.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SPMModules",
+    platforms: [.iOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -17,18 +18,18 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/sf-jed-kyung/spm-chat",
-            from: "1.0.2"
+            from: "1.0.3"
         ),
     ],
     targets: [
         .binaryTarget(
             name: "SPMTemplate",
-            url: "https://github.com/sf-jed-kyung/spm-modules/releases/download/1.0.4/SPMTemplate.xcframework.zip",
+            url: "https://github.com/sf-jed-kyung/spm-modules/releases/download/1.0.5/SPMTemplate.xcframework.zip",
             checksum: "577162bfe0a691828a8008ee3875e69b71924e5d2ba1ebe6456f768a24aa1842"
         ),
         .binaryTarget(
             name: "SPMUIKit",
-            url: "https://github.com/sf-jed-kyung/spm-modules/releases/download/1.0.4/SPMUIKit.xcframework.zip",
+            url: "https://github.com/sf-jed-kyung/spm-modules/releases/download/1.0.5/SPMUIKit.xcframework.zip",
             checksum: "2333a1e69ba4d32db656bd13cac6fe49886f3741fae76437d8f9eeebce1c1704"
         ),
         
