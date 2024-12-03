46472be4f26369bfbe77848ee8138d82ba555313
Jed Gyeong <jed@sendbird.com>
2024-12-03 09:59:12 +0900

    1.0.11

--- Package.swift
+++ Package.swift
@@ -17,9 +17,9 @@ let package = Package(
         .library(
             name: "SPMOpenChannel",
             targets: ["SPMOpenChannelTarget"]),
-//        .library(
-//            name: "SPMGroupChannel",
-//            targets: ["SPMGroupChannelTarget"]),
+        .library(
+            name: "SPMGroupChannel",
+            targets: ["SPMGroupChannelTarget"]),
 //        .library(
 //            name: "SPMBusinessMessaging",
 //            targets: ["SPMBusinessMessagingTarget"]),
@@ -33,12 +33,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SPMTemplate",
-            url: "https://github.com/sf-jed-kyung/spm-modules/releases/download/1.0.10/SPMTemplate.xcframework.zip",
+            url: "https://github.com/sf-jed-kyung/spm-modules/releases/download/1.0.11/SPMTemplate.xcframework.zip",
             checksum: "577162bfe0a691828a8008ee3875e69b71924e5d2ba1ebe6456f768a24aa1842"
         ),
         .binaryTarget(
             name: "SPMUIKit",
-            url: "https://github.com/sf-jed-kyung/spm-modules/releases/download/1.0.10/SPMUIKit.xcframework.zip",
+            url: "https://github.com/sf-jed-kyung/spm-modules/releases/download/1.0.11/SPMUIKit.xcframework.zip",
             checksum: "29c2e36730d77361bc67838271d15d90aa65719268592636bc1ffac721b5ce82"
         ),
         
@@ -77,17 +77,17 @@ let package = Package(
             ]
         ),
         
-//        .target(
-//            name: "SPMGroupChannelTarget",
-//            dependencies: [
-//                .product(name: "SPMChat", package: "spm-chat")
-//            ],
-//            path: "Framework/SPMGroupChannel",
-//            resources: [
-//               .copy("Resources/PrivacyInfo.xcprivacy"),
-//            ]
-//        ),
-//        
+        .target(
+            name: "SPMGroupChannelTarget",
+            dependencies: [
+                .product(name: "SPMChat", package: "spm-chat")
+            ],
+            path: "Framework/SPMGroupChannel",
+            resources: [
+               .copy("Resources/PrivacyInfo.xcprivacy"),
+            ]
+        ),
+        
 //        .target(
 //            name: "SPMBusinessMessagingTarget",
 //            dependencies: [
