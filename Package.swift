7340e4cf6b214c35ff842a7ce8f33c97d891c2f8
Jed Gyeong <jed@sendbird.com>
2024-12-03 10:18:59 +0900

    1.0.12

--- Package.swift
+++ Package.swift
@@ -20,9 +20,9 @@ let package = Package(
         .library(
             name: "SPMGroupChannel",
             targets: ["SPMGroupChannelTarget"]),
-//        .library(
-//            name: "SPMBusinessMessaging",
-//            targets: ["SPMBusinessMessagingTarget"]),
+        .library(
+            name: "SPMBusinessMessaging",
+            targets: ["SPMBusinessMessagingTarget"]),
     ],
     dependencies: [
         .package(
@@ -33,12 +33,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SPMTemplate",
-            url: "https://github.com/sf-jed-kyung/spm-modules/releases/download/1.0.11/SPMTemplate.xcframework.zip",
+            url: "https://github.com/sf-jed-kyung/spm-modules/releases/download/1.0.12/SPMTemplate.xcframework.zip",
             checksum: "577162bfe0a691828a8008ee3875e69b71924e5d2ba1ebe6456f768a24aa1842"
         ),
         .binaryTarget(
             name: "SPMUIKit",
-            url: "https://github.com/sf-jed-kyung/spm-modules/releases/download/1.0.11/SPMUIKit.xcframework.zip",
+            url: "https://github.com/sf-jed-kyung/spm-modules/releases/download/1.0.12/SPMUIKit.xcframework.zip",
             checksum: "29c2e36730d77361bc67838271d15d90aa65719268592636bc1ffac721b5ce82"
         ),
         
@@ -88,16 +88,16 @@ let package = Package(
             ]
         ),
         
-//        .target(
-//            name: "SPMBusinessMessagingTarget",
-//            dependencies: [
-//                .product(name: "SPMChat", package: "spm-chat")
-//            ],
-//            path: "Framework/SPMBusinessMessaging",
-//            resources: [
-//               .copy("Resources/PrivacyInfo.xcprivacy"),
-//            ]
-//        ),
+        .target(
+            name: "SPMBusinessMessagingTarget",
+            dependencies: [
+                .product(name: "SPMChat", package: "spm-chat")
+            ],
+            path: "Framework/SPMBusinessMessaging",
+            resources: [
+               .copy("Resources/PrivacyInfo.xcprivacy"),
+            ]
+        ),
     ],
     cxxLanguageStandard: .cxx11
 )
